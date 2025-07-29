14eb16570457e6489dbc40372fd2942e8e692361
fureiyaa <farizdzu16@gmail.com>
2025-07-29 12:08:53 +0700

    ubae

---
+++ resources/views/login.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
+</head>
+<body class="bg-light min-vh-100 d-flex flex-column">
+    <div class="container mt-5 text-white p-5 rounded">
+        <div class="card d-flex justify-content-center align-items-center mx-auto" style="width: 500px; height: 400px;">
+            <div class="mt-5">
+                <h2>Welcome To</h2>
+                <h2>Our Dues</h2>
+            </div>
+            <div class="card-body" style="height: 200px; width: 400px; justify-content: center; align-items: center; display: flex; flex-direction: column;">
+                <input type="text" class="form-control mb-3" placeholder="Username">
+                <input type="text" class="form-control mb-3" placeholder="Password">
+                <div class="container d-flex flex-column justify-content-center align-items-center gap-2">
+                    <a href="" class="btn btn-primary" style="width: 350px">Login</a>
+                    <a href="" class="btn btn-secondary" style="width: 350px">Register</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="text-center mt-5">
+        <p>&copy; {{ date('Y') }} Iuran Warga. All rights reserved.</p>
+    </footer>
+</body>
+<script src="{{ asset('assets/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+</html>
