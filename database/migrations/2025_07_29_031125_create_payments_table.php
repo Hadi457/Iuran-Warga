11b115a9ee70f9b5d8ba5f9705661108e941e379
fureiyaa <farizdzu16@gmail.com>
2025-07-29 10:44:23 +0700

    ubae

--- database/migrations/2025_07_29_031125_create_payments_table.php
+++ database/migrations/2025_07_29_031125_create_payments_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('iduser')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->enum('period',['harian', 'bulanan', 'tahunan']);
+            $table->integer('nominal');
+            $table->string('petugas');
             $table->timestamps();
         });
     }
--- database/migrations/2025_07_29_031215_create_officers_table.php
+++ database/migrations/2025_07_29_031215_create_officers_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('officers', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('iduser')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
         });
     }
