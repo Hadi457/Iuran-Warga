225498c54be97e3c2be88acca89ce9dcc527c534
fureiyaa <farizdzu16@gmail.com>
2025-08-14 09:24:00 +0700

    ubah

--- resources/views/Administrator/payment.blade.php
+++ resources/views/Administrator/payment.blade.php
@@ -1,18 +1,43 @@
-@extends('Administrator.template')
+>@extends('Administrator.template')
 @section('content')
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    <h4>Payment Management</h4>
-                </div>
-                <div class="card-body">
-                    <p>Manage your payments here.</p>
-                    <!-- Add your payment management content here -->
-                </div>
-            </div>
-        </div>
+    <div class="container" style="width: 1000px; margin-top: 20px;">
+        <h1>Data Tagihan</h1>
     </div>
-</div>
+    <div class="container mt-5">
+        <form action="{{ url('/cari') }}" method="GET" class="d-flex mb-3" style="width: 300px">
+            <input type="text" name="q" class="form-control me-2" placeholder="Cari data..." value="{{ request('q') }}">
+            <button type="submit" class="btn btn-primary">Cari</button>
+        </form>
+
+        <table class="table table-bordered table-striped mt-3">
+            <thead class="table-success">
+                <tr>
+                    <th>ID</th>
+                    <th>NIK</th>
+                    <th>Nama</th>
+                    <th>Username</th>
+                    <th>Alamat</th>
+                    <th>No. Telepon</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            {{-- @foreach ($warga as $item)
+            <tbody>
+                <tr>
+                    <td scope="row">{{ $item->id }}</td>
+                    <td>{{$item->nik}}</td>
+                    <td>{{ $item->name }}</td>
+                    <td>{{ $item->user->username }}</td>
+                    <td>{{ $item->addres }}</td>
+                    <td>{{ $item->number_handphone }}</td>
+                    <td>{{ $item->user->level}}</td>
+
+                    <td>
+                        <a class="btn btn-warning" href="{{route('tagihan.warga', Crypt::encrypt($item->id))}}">Lihat Tagihan</a>
+                    </td>
+                </tr>
+            </tbody>
+            @endforeach --}}
+        </table>
+    </div>
 @endsection
