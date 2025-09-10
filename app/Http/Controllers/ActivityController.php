01fc66a9d496d32b85de60e5c73614e696f845e1
Hadi457 <muhamadkhoirulhadi567@gmail.com>
2025-09-10 11:28:34 +0700

    Activity

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ActivityController extends Controller
 {
     public function index()
     {
-        return view('activity');
+        if (Auth::user()->level == 'Admin') {
+            return view('Administrator.activity');
+        } else if (Auth::user()->level == 'Warga') {
+            return view('activity');
+        } else {
+            return redirect()->route('login')->with('error', 'You do not have access to this page.');
+        }
     }
 }
